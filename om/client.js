9d9f1b517dac374f555696f01fdacdaaa58a9b7d
wechu07 <wechuli017@gmail.com>
2022-08-11 15:07:10 +0300

    node-redis connection

--- om/client.js
+++ om/client.js
@@ -1,12 +1,21 @@
 import { Client } from 'redis-om'
+import { createClient } from 'redis'
 
 /* pulls the Redis URL from .env */
 const url = process.env.REDIS_URL
 
+/* create a connection to Redis with Node Redis */
+export const connection = createClient({ url })
+await connection.connect()
+
 /* create and open the Redis OM Client */
 // the .open() method conveniently returns `this`
 // chains the instantiation of the client with the opening of the client
-const client = await new Client().open(url)
+
+
+/* create a Client and bind it to the Node Redis connection */
+const client = await new Client().use(connection)
+client.on('error', (err) => console.error(err))
 // client.on('connect', function () {
 //     console.log('Redis database onnected!');
 // }); //redis db connected status
